a8e48316fbdd5cd5680d20bc2b2005f46595e4da
Jairo Aguirre <jaguirrebe@uoc.edu>
2024-11-03 13:45:20 +0100

    03/NOV

---
+++ datos.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(SummarizedExperiment)
+data <- read_excel("~/MÁSTER/ANÁLISIS DATOS ÓMICOS/PEC1/GastricCancer_NMR.xlsx", sheet = "Data")
+peak <- read_excel("~/MÁSTER/ANÁLISIS DATOS ÓMICOS/PEC1/GastricCancer_NMR.xlsx", sheet = "Peak")
+assay_data <- as.matrix(data[, grep("^M", names(data))])
+se <- SummarizedExperiment(assays = list(counts = assay_data), 
+                           colData = peak)
+se
+row_data <- data[, c("Idx", "SampleID", "SampleType", "Class")]
+se <- SummarizedExperiment(assays = list(counts = assay_data), 
+                             rowData = row_data,
+                             colData = peak)
+
+se
+class(colData(se))
