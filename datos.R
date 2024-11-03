7666dc284e60ee9701d79706cbd9bb276f1efd92
Jairo Aguirre <jaguirrebe@uoc.edu>
2024-11-03 14:20:44 +0100

    datos RDA

--- datos.R
+++ datos.R
@@ -3,9 +3,6 @@ library(SummarizedExperiment)
 data <- read_excel("~/MÁSTER/ANÁLISIS DATOS ÓMICOS/PEC1/GastricCancer_NMR.xlsx", sheet = "Data")
 peak <- read_excel("~/MÁSTER/ANÁLISIS DATOS ÓMICOS/PEC1/GastricCancer_NMR.xlsx", sheet = "Peak")
 assay_data <- as.matrix(data[, grep("^M", names(data))])
-se <- SummarizedExperiment(assays = list(counts = assay_data), 
-                           colData = peak)
-se
 row_data <- data[, c("Idx", "SampleID", "SampleType", "Class")]
 se <- SummarizedExperiment(assays = list(counts = assay_data), 
                              rowData = row_data,
@@ -20,3 +17,9 @@ head(assay(se))
 head(rowData(se))
 colData(se)[1, ]
 colData(se)[6, ]
+hist(peak$Perc_missing, main="Distribución de Datos Faltantes", 
+     xlab="Porcentaje de Datos Faltantes", 
+     ylab="Frecuencia", col="pink")
+boxplot(peak$QC_RSD, main="Distribución de QC_RSD", 
+        ylab="RSD (%)", col="violet")
+save(se, file = "dataset_metabolomica.Rda")
